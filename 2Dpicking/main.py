9c7d8ff35fa0d20d890b915cd70aecbed3c7a7e9
Yifei Li <52587271+LI-SUSTech@users.noreply.github.com>
2021-03-24 17:55:27 +0800

    Add files via upload
    main.py for 2D picking

---
+++ 2Dpicking/main.py
@@ -0,0 +1,148 @@
+# MIT License.
+# Copyright (c) 2021 by BioicDL. All rights reserved.
+# Created by LiuXb on 2021/1/31
+# -*- coding:utf-8 -*-
+
+"""
+@Modified: 
+@Description:
+"""
+import sys
+import os
+# append the DeepClawDev directory to python path and set it as working directory
+_root_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(_root_path)
+os.chdir(_root_path)
+print('work_dir: ', _root_path) #显示工作路径
+
+import time
+import yaml
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from scipy.spatial.transform import Rotation as R
+
+from deepclaw.driver.arms.ArmController import ArmController
+from deepclaw.modules.end2end.yolov5.YOLO5 import Yolo5
+from deepclaw.driver.sensors.camera.Realsense_L515 import Realsense
+
+from deepclaw.modules.calibration.Calibration2D import Calibration2D
+from deepclaw.driver.arms.franka.FrankaController import FrankaController
+
+
+def pick_place(robot_server: ArmController, gripper_server: object, home_joint, pick_xyzrt, place_xyzrt):
+    # go to pick above，到抓取的物体上方
+    up_pos = pick_xyzrt.copy()
+    up_pos[2] = up_pos[2] + 0.1
+
+    robot_server.move_p(up_pos, 1.5, 1.5)
+    time.sleep(0.01)
+    # go to pick
+    robot_server.move_p(pick_xyzrt, 1.5, 1.5)
+    # pick
+    #gripper_server.set_tool_out(0, True)
+    time.sleep(1)
+    # go up
+    robot_server.move_p(up_pos, 1.5, 1.5)
+    time.sleep(0.01)
+    print('##############################')
+
+    # go to release
+    robot_server.move_p(place_xyzrt, 2.5, 2.5)
+    print('##############################')
+    # release
+    #gripper_server.set_tool_out(0, False)
+    time.sleep(0.8)
+    # go back home
+    robot_server.move_j(home_joint, 1.5, 1.5)
+    time.sleep(0.01)
+
+
+def detectObject(detector_algo: Yolo5, crop_bounding=[340, 700, 340, 1200]):
+    region_class = detector_algo.detect(color)
+    if region_class is None:
+        return False, None, None, None
+    print(crop_bounding)
+    # object on the tray                                                       这三个是什么？？？？？？？？？？？
+    uv_roi = []
+    cla_roi = []
+    cfi_roi = []
+    for i in range(len(region_class)):
+        uv_temp = np.array(region_class[i][0:4], np.int16)
+        cla_temp = int(region_class[i][5])
+        cfi_temp = region_class[i][4]
+        # col_min, row_min, col_max, row_max
+        uv_mid_c = (uv_temp[0]+uv_temp[2])/2.0
+        uv_mid_r = (uv_temp[1]+uv_temp[3])/2.0
+        if uv_mid_c < crop_bounding[2] - 20 or uv_mid_c > crop_bounding[3]+10 or uv_mid_r < crop_bounding[0] - 20 or uv_mid_r > crop_bounding[1] + 10:
+            continue
+        else:
+            uv_roi.append(uv_temp)
+            cla_roi.append(cla_temp)
+            cfi_roi.append(cfi_temp)
+
+    if len(uv_roi) == 0:
+        return False, None, None, None
+
+    # pick one object once
+    uv = uv_roi[0]
+    cla = cla_roi[0]
+    cfi = cfi_roi[0]
+    return True, uv, cla, cfi
+
+
+if __name__ == '__main__':
+    """ Initialization """
+    # camera and robot driver
+    robot = FrankaController('./configs/basic_config/franka.yaml')
+    camera = Realsense('./configs/basic_config/camera_rs_d435.yaml')
+    object_detector = Yolo5 ('./configs/basic_config/yolov5_cfg.yaml')
+
+    home_joints = [-0.0347, -0.6137, 0.0812, -2.175, 0.0849, 1.7349, 0.4538]
+    robot.move_j(home_joints, 1.5, 1.5)
+
+    """ start picking loop"""
+    place_xyzrt = [0.3, 0.0, 0.4, 3.14, 0.0, 0.0]
+    crop_bounding = [340, 700, 340, 1200]
+    cali_path = './configs/basic_config/cali2D.yaml'
+
+    show_rect_flag = 0
+    if show_rect_flag:
+        frame = camera.get_frame()
+        color = frame.color_image[0]
+        xx = color[crop_bounding[0]:crop_bounding[1], crop_bounding[2]:crop_bounding[3]]
+        fig = plt.figure("rect")
+        plt.imshow(xx)
+        plt.show()
+
+    while 1:
+        frame = camera.get_frame()
+        color = frame.color_image[0]
+        # 识别物体
+        # region_class = object_detector.detect(color)
+        ret, uv, cla, cfi = detectObject(object_detector, crop_bounding)
+        if not ret:
+            continue
+        if cla not in [0, 1, 2, 3]:
+            print('\033[1;35m Error Category \033[0m!')
+            continue
+        """ generate grasping pose"""
+        hand_eye = Calibration2D(cali_path)
+        # col
+        ux = (uv[0] + uv[2]) / 2.0
+        # row
+        vy = (uv[1] + uv[3]) / 2.0
+        temp = hand_eye.cvt(ux, vy)                 #？？？？？？？？？？？？？
+        z = 0.2
+        if abs(uv[2] - uv[0]) >= abs(uv[3] - uv[1]):
+            angle = 0.0
+        else:
+            angle = -1.57
+        # grasp pose in euler angle
+        temp_pose = [temp[0], temp[1], z, 3.14, 0, angle]
+        # transfer to rotation vector
+        r = R.from_euler('xyz', temp_pose[3:6], degrees=False)
+        rvc = r.as_rotvec()
+        pick_pose = [temp_pose[0], temp_pose[1], temp_pose[2], rvc[0], rvc[1], rvc[2]]
+        # grasping
+        pick_place(robot, robot, home_joints, pick_pose, place_xyzrt)
